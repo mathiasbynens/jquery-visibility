00ab51fa37dafa0aca36963e34e76212159c0d7f
Jan Paepke <e-mail@janpaepke.de>
2015-04-10 13:29:41 +0200

    built v1.0.9

--- jquery-visibility.js
+++ jquery-visibility.js
@@ -1,17 +1,41 @@
-/*! http://mths.be/visibility v1.0.8 by @mathias | MIT license */
-;(function(window, document, $, undefined) {
+/*!
+ * jquery-visibility v1.0.8
+ * Page visibility shim for jQuery.
+ *
+ * Project Website: http://mths.be/visibility
+ * 
+ * @version 1.0.8
+ * @license MIT.
+ * @author Mathias Bynens - @mathias
+ * @author Jan Paepke - @janpaepke
+ */
+;(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], function ($) {
+        	return factory(root, $);
+        });
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS
+        module.exports = factory(root, require('jquery'));
+    } else {
+        // Browser globals
+        factory(root, jQuery);
+    }
+}(this, function(window, $, undefined) {
 	"use strict";
 
-	var prefix;
-	var property;
-	// In Opera, `'onfocusin' in document == true`, hence the extra `hasFocus` check to detect IE-like behavior
-	var eventName = 'onfocusin' in document && 'hasFocus' in document ?
-		'focusin focusout' :
-		'focus blur';
-	var prefixes = ['webkit', 'o', 'ms', 'moz', ''];
-	var $support = $.support;
-	var $event = $.event;
+	var
+		document = window.document,
+		property, // property name of document, that stores page visibility
+		vendorPrefixes = ['webkit', 'o', 'ms', 'moz', ''],
+		$support = $.support || {},
+		// In Opera, `'onfocusin' in document == true`, hence the extra `hasFocus` check to detect IE-like behavior
+		eventName = 'onfocusin' in document && 'hasFocus' in document ?
+			'focusin focusout' :
+			'focus blur';
 
+	var prefix;
 	while ((prefix = prefixes.pop()) !== undefined) {
 		property = (prefix ? prefix + 'H': 'h') + 'idden';
 		$support.pageVisibility = document[property] !== undefined;
@@ -52,7 +76,7 @@
 				originalEvent.relatedTarget === undefined
 			)
 		) {
-			$event.trigger(
+			$(document).triggerHandler(
 					property && document[property] || /^(?:blur|focusout)$/.test(type) ?
 						'hide' :
 						'show'
@@ -61,5 +85,4 @@
 		// and update the current state
 		updateState();
 	});
-
-}(this, document, jQuery));
+}));
